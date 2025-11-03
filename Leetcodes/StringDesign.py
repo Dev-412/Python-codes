0e7483d3718bae744537e994740af37fc3271c0c
Dev-412 <dev30112010@gmail.com>
2025-11-04 00:09:19 +0530

    updated 2nd method

--- Leetcodes/StringDesign.py
+++ Leetcodes/StringDesign.py
@@ -52,3 +52,40 @@ print("")
 c=triple[2::3]
 c=" ".join(c)
 print(c)
+
+
+
+
+
+
+
+
+
+
+# 2nd methodddd
+
+x= "goodmorning"
+y=x[3::4]
+a=0
+for i in (x[::4]):
+    print(i,end="")
+    if(a==len(x[::4])-1):
+        break
+    print(" ",end="")
+    a=a+1
+print("")
+a=0
+for i in (x[1::4]):
+    print(i,end="")
+    if(a==len(x[::4])-1):
+        break
+    print(y[a],end="")
+    a=a+1
+print("")
+a=0
+for i in (x[2::4]):
+    print(i,end="")
+    if(a==len(x[::4])-1):
+        break
+    print(" ",end="")
+    a=a+1
